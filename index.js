2dec6d9a17c3a674b37b2eaedd74397895210065
Jon Kuperman <jon.kuperman@gmail.com>
2024-05-18 13:55:07 +0100

    got it working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const acorn = require("acorn");
+const escodegen = require("escodegen");
+const { transformVarToLetOrConst } = require("./transform");
 
 const inputFilePath = "input.js";
 const inputCode = fs.readFileSync(inputFilePath, "utf-8");
@@ -9,4 +11,14 @@ const ast = acorn.parse(inputCode, {
   sourceType: "module",
 });
 
-console.log(JSON.stringify(ast, null, 2));
+// Apply transformations
+transformVarToLetOrConst(ast);
+
+// Generate the modernized code
+const modernizedCode = escodegen.generate(ast);
+console.log(modernizedCode);
+
+// Write the modernized code to a new file
+const outputFilePath = "output.js";
+fs.writeFileSync(outputFilePath, modernizedCode, "utf-8");
+console.log(`Modernized code written to ${outputFilePath}`);
--- transform.js
+++ transform.js
@@ -1,20 +1,15 @@
 const estraverse = require("estraverse");
-const escodegen = require("escodegen");
 
-// Traverse and transform the AST
-estraverse.replace(ast, {
-  enter(node) {
-    if (node.type === "VariableDeclaration" && node.kind === "var") {
-      node.kind = "let"; // For simplicity, we use 'let' here
-    }
-  },
-});
+function transformVarToLetOrConst(ast) {
+  estraverse.replace(ast, {
+    enter(node) {
+      if (node.type === "VariableDeclaration" && node.kind === "var") {
+        node.kind = "let"; // For simplicity, we use 'let' here
+      }
+    },
+  });
+}
 
-// Generate the modernized code
-const modernizedCode = escodegen.generate(ast);
-console.log(modernizedCode);
-
-// Write the modernized code to a new file
-const outputFilePath = "output.js";
-fs.writeFileSync(outputFilePath, modernizedCode, "utf-8");
-console.log(`Modernized code written to ${outputFilePath}`);
+module.exports = {
+  transformVarToLetOrConst,
+};
